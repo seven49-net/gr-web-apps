e98648db0b99300668b9776ca01fee93e88d9931
Hans-Jakob Bollinger <contact@webbear.ch>
2021-04-14 15:16:27 +0200

    addresses gateway index-postleizahl getter

--- api-gateway-dynamodb-addresses/index.js
+++ api-gateway-dynamodb-addresses/index.js
@@ -2,69 +2,87 @@ const AWS = require('aws-sdk');
 
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
-/**
- * Demonstrates a simple HTTP endpoint using API Gateway. You have full
- * access to the request and response payload, including headers and
- * status code.
- *
- * To scan a DynamoDB table, make a GET request with the TableName as a
- * query string parameter. To put, update, or delete an item, make a POST,
- * PUT, or DELETE request respectively, passing in the payload to the
- * DynamoDB API as a JSON body.
- */
 exports.handler = async (event, context) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('Received event:', JSON.stringify(event, null, 2));
 
-    let body;
-    let statusCode = '200';
-    const headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    };
+  let body;
+  let statusCode = '200';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  };
 
-    let params;
-    let qs = event.queryStringParameters;
+  let params;
+  let qs = event.queryStringParameters;
+
+  // https://vdk8qyhmsa.execute-api.eu-west-1.amazonaws.com/PROD/?index_name=index-postleitzahl&rec_art_name=plz1&searchterm=3000
+
+  try {
+    switch (event.httpMethod) {
+      case "GET":
+        let indexName = qs.hasOwnProperty("index_name") ? qs.index_name.toUpperCase() : null;
+        let primaryKeyValue = qs.hasOwnProperty("rec_art_name") ? qs.rec_art_name.toUpperCase() : null;
+        let searchterm = qs.hasOwnProperty("searchterm") ? qs.searchterm : null;
 
 
-    try {
-        switch (event.httpMethod) {
-          case "GET":
-          if (qs.hasOwnProperty("index_name")) {
-            let indexName = qs.index_name.toUpperCase();
-            // let key = qs.key.toUpperCase();
-            let search = qs.search;
-            params = {
-              TableName: "gr_post_adressdaten",
-              IndexName: "INDEX-POSTLEITZAHL",
-              KeyConditionExpression: "#rec = :r and #plz = :s",
-              ExpressionAttributeNames:{
-                "#rec": "REC_ART_NAME",
-                "#plz": "POSTLEITZAHL"
-            },
-              ExpressionAttributeValues: {
-                ":s": 3000,
-                ":r": "PLZ1"
-              }
-            }
-          }
-          break;
-            default:
-              params = {
-                TableName: "gr_post_adressdaten"
-              }
-
+        if (indexName && primaryKeyValue) {
+          params = buildParameters(indexName, primaryKeyValue, searchterm);
         }
-        body = await dynamo.query(params).promise();
-    } catch (err) {
-        statusCode = '400';
-        body = err.message;
-    } finally {
-        body = JSON.stringify(body);
-    }
+        break;
+      default:
+        params = {
+          TableName: "gr_post_adressdaten"
+        }
 
-    return {
-        statusCode,
-        body,
-        headers,
-    };
+    }
+    body = await dynamo.query(params).promise();
+  } catch (err) {
+    statusCode = '400';
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 };
+
+function buildParameters(iName, recArtName, value) {
+
+  let keyConditionExpression = "#prim = :p and #sec = :s";
+  let secondaryKey = '';
+  let s = value;
+
+  switch (iName.toUpperCase()) {
+    case "INDEX-POSTLEITZAHL":
+      keyConditionExpression = "#prim = :p and #sec = :s";
+      secondaryKey = "POSTLEITZAHL";
+      s = parseInt(value);
+      break;
+
+    default:
+      keyConditionExpression = "#prim = :p and #sec = :s";
+      secondaryKey: "POSTLEITZAHL";
+      s = parseInt(value);
+      break;
+  }
+
+
+  let params = {
+    TableName: "gr_post_adressdaten",
+    IndexName: iName.toUpperCase(),
+    KeyConditionExpression: keyConditionExpression,
+    ExpressionAttributeNames: {
+      "#prim": "REC_ART_NAME",
+      "#sec": secondaryKey
+    },
+    ExpressionAttributeValues: {
+      ":s": s,
+      ":p": recArtName
+    }
+  };
+  return params;
+}
