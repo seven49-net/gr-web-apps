3566b12cd2d0644fef9174a3e3f1b9142580d15a
Hans-Jakob Bollinger <contact@webbear.ch>
2024-04-26 09:01:53 +0200

    address-check: add html tags to messages

--- address-check/src/js/messages.js
+++ address-check/src/js/messages.js
@@ -4,19 +4,11 @@ const messages = {
     house_no_missing: `Um die Adresse zu verifizieren, wird noch die Hausnummer benötigt.`,
     no_verfication: `Die eingegebene Adresse ({streetName} {houseNumber}, {zipcode} {city}) konnte leider nicht verifiziert werden!`,
     check_zipcode:
-      "Bitte korrigieren Sie die PLZ von {uzipcode} auf {pzipcode}.",
+      "Bitte korrigieren Sie die PLZ von <s>{uzipcode}</s> auf <em>{pzipcode}</em>.",
     check_townname:
-      "Bitte die Schreibweise der Ortschaft von {utownname} auf {ptownname} anpassen.",
+      "Bitte die Schreibweise der Ortschaft von <s>{utownname}</s> auf <em>{ptownname}</em> anpassen.",
     check_streetname:
-      "Bitte die Schreibweise der Strasse von {ustreetname})  auf {pstreetname} anpassen.",
-    street_not_found:
-      'Die Strasse "{street}" wurde nicht gefunden unter der PLZ {plz} - bitte prüfen Sie, ob die Adressdaten alle korrekt ausgefüllt sind.',
-    no_ch_plz: "{plz} ist keine Schweizer Postleitzahl",
-    success:
-      'Die Strasse "{street}" wurde unter der angegebenen PLZ {plz} gefunden!',
-    enter_valid_plz: "Bitte geben Sie eine gültige Postleitzahl ein!",
-    plz_city_not_conform: `Die Postleitzahl {plz} stimmt nicht mit dem Ort {city} überein!`,
-    state_not_correct: `Bitte verwenden Sie für die Angabe des Kantons die Abkürzung.<br>Richtig: <i>{state}</i><br>Eingegeben: <i>{entered_state}</i>.`,
+      "Bitte die Schreibweise der Strasse von <s>{ustreetname}</s>  auf <em>{pstreetname}</em> anpassen.",
   },
 };
 
@@ -83,7 +75,7 @@ function successMsg(content) {
 function div(txt = "", c = "") {
   const d = document.createElement("div");
   d.classList.add(c);
-  d.textContent = txt;
+  d.innerHTML = txt;
   return d;
 }
 
