597e3b0ae3bcab17448dafc60348f4afe3997d22
Hans-Jakob Bollinger <contact@webbear.ch>
2024-09-06 08:27:06 +0200

    stellenportal-new: fix css, adjust vite config

--- stellenportal-new/app/vite.config.js
+++ stellenportal-new/app/vite.config.js
@@ -4,12 +4,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [vue()],
+      base: '/',
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      }
+    }
+  } else {
+    return {
+      plugins: [vue()],
+      base: 'https://cdn.gr.ch/apps/int/stellenportal-test/',
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL(',/src', import.meta.url))
+        }
+      }
     }
   }
 })
