9d2de8399cdfb69cd1684ff37de6f7a01625a891
Hans-Jakob Bollinger <contact@webbear.ch>
2024-10-10 08:58:49 +0200

    remove vite-plugin-entry-inject and add plugin method to vite config

--- stellenportal-new/app/vite.config.js
+++ stellenportal-new/app/vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VitePluginEntryInject from 'vite-plugin-entry-inject'
+// import VitePluginEntryInject from 'vite-plugin-entry-inject'
 
 // https://vitejs.dev/config/
 /*
@@ -34,7 +34,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
 export default defineConfig({
   plugins: [
     vue(),
-    VitePluginEntryInject({
+    vitePluginEntryInject({
       // head-prepend/head/body-prepend/body
       injectTo: 'body'
     })
@@ -46,3 +46,50 @@ export default defineConfig({
     }
   }
 })
+
+function vitePluginEntryInject(config) {
+  return {
+    name: 'rollup-plugin-entry-inject',
+    transformIndexHtml(html, ctx) {
+      const { injectTo = 'body' } = config
+
+      let script = ''
+
+      if (ctx.chunk) {
+        html = html.replace(
+          new RegExp('<script[^<>]+' + ctx.chunk.fileName + '[^<>]+></script>'),
+          (match) => {
+            script = match
+
+            return ''
+          }
+        )
+
+        const injectMap = {
+          head: {
+            tag: '</head>',
+            template: `${script}</head>`
+          },
+          'head-prepend': {
+            tag: '<head>',
+            template: `<head>${script}`
+          },
+          body: {
+            tag: '</body>',
+            template: `${script}</body>`
+          },
+          'body-prepend': {
+            tag: '<body>',
+            template: `<body>${script}`
+          }
+        }
+
+        html = html.replace(injectMap[injectTo].tag, injectMap[injectTo].template)
+        const coPat = /crossorigin/g
+        html = html.replace(coPat, '')
+      }
+
+      return html
+    }
+  }
+}
